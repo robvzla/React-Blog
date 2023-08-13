2b0386210a9f1a2e5ab8623e03601063825f2e61
roberto cassino <robertocassino@student.griffith.ie>
2023-08-13 14:29:19 +0100

    Adding project

---
+++ Frontend/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticleListPage from './pages/ArticlesListPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFound';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <NavBar /> 
+      <div id="page-body">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/articles' element={<ArticleListPage />} />
+          <Route path='/articles/:articleId' element={<ArticlePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/create-account' element={<CreateAccountPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Frontend/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZBV7zh5krGdsXe58qIM_5xBEYtCZEiRI",
+  authDomain: "personal-blog-a40d3.firebaseapp.com",
+  projectId: "personal-blog-a40d3",
+  storageBucket: "personal-blog-a40d3.appspot.com",
+  messagingSenderId: "48281036704",
+  appId: "1:48281036704:web:2863d7ddf36f408862e6c5",
+  measurementId: "G-9071KJVG5B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
